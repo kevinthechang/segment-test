2f32cf1e633541c75a8422517c67689257953a6b
kevinthechang <kchang85@gmail.com>
2015-07-26 15:43:06 -0700

    updated segment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,11 @@ class PagesController < ApplicationController
 	def about
 		# intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
   		# intercom.events.create(event_name: "Click About", created_at: Time.now.to_i, email: current_user.email)
+  		Analytics.track(
+		  user_id: current_user.user_id,
+		  event: 'Clicked About',
+		  properties: {}
+		)
 	end
 	def thanks
 	end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,11 @@ class SessionsController < ApplicationController
     		session[:user_id] = @user.id
     		# intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
   			# intercom.events.create(event_name: "Logged In", created_at: Time.now.to_i, email: @user.email)
+        Analytics.track(
+          user_id: @user.id,
+          event: 'Logged In',
+          properties: {}
+        )
     		redirect_to '/content'
   		else
     		redirect_to '/login'
