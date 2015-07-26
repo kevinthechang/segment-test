e1f394252b5afd2f117cd25d2d617a723d4b5be1
kevinthechang <kchang85@gmail.com>
2015-07-25 18:29:22 -0700

    Initial Commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+	before_action :require_user, only: [:content, :about]
+	def home
+	end
+	def about
+		# intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
+  		# intercom.events.create(event_name: "Click About", created_at: Time.now.to_i, email: current_user.email)
+	end
+	def thanks
+	end
+	def content
+		# intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
+  		# count = intercom.counts.for_type(type: 'user', count: 'segment')
+  		# @new_user_count = count.user["segment"][3]["New"]
+	end
+
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+	def create
+  		@user = User.find_by_email(params[:session][:email])
+  		if @user && @user.authenticate(params[:session][:password])
+    		session[:user_id] = @user.id
+    		# intercom = Intercom::Client.new(app_id: 'umxbi8zj', api_key: '1da89d0c08354cc43301eca6bec0b25188903c41')
+  			# intercom.events.create(event_name: "Logged In", created_at: Time.now.to_i, email: @user.email)
+    		redirect_to '/content'
+  		else
+    		redirect_to '/login'
+  		end 
+	end
+  	def destroy
+    	session[:user_id] = nil
+    	redirect_to '/'
+  	end
+end
